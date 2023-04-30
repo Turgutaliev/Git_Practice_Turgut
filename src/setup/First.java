f590ba06c988c85c4a3c73ddb05080e6401dc550
turgutaliev <turgutaliev@gmail.com>
2023-04-30 13:04:11 -0400

    Initial commit

---
+++ src/setup/First.java
@@ -0,0 +1,13 @@
+package setup;
+
+public class First {
+    public static void main(String[] args) {
+
+        System.out.println("Hello universe");
+
+        // this is a new line
+
+        // THIS IS THIRD FILE
+
+    }
+}
